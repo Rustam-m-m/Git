099f2f196580d6b0d917465a2411ba22444040aa
Rustam Tukhvatov <rustam_m_m@mail.ru>
2014-03-22 17:03:50 +0400

    Initial Matrix project

---
+++ Matrix/src/Matrix.java
@@ -0,0 +1,9 @@
+/**
+ * Created by ww on 15.03.14.
+ */
+public interface Matrix {
+    int getValue(int row, int column);
+    void setValue(int row, int column, int value);
+    String toString();
+
+}
---
+++ Matrix/src/RunnerClass.java
@@ -0,0 +1,19 @@
+/**
+ * Created by ww on 15.03.14.
+ */
+import ru.kpfu.npp.math.MatrixArrayImpl;
+
+public class RunnerClass {
+    public static void main(String args[])
+    {
+        MatrixArrayImpl matrix;
+        matrix = new MatrixArrayImpl();
+        for(int i = 0; i< 30 ; i++){
+            for(int j = 0; j < 30; j++){
+                matrix.setValue(i, j, i+j);
+            }
+    }
+    Object object = new Object();
+    System.out.println(matrix);
+}
+}
